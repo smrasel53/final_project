abba3d1cb6b10bd7f7d1616cbc0493b1088d7031
smrasel53 <smrasel.cse@gmail.com>
2017-03-03 16:12:24 +0600

    online exam system final project

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+		<div class="well">
+			<h4>Copyright &copy; Mindset Group | All Rights Reserved.
+				<span class="pull-right">Developed By <a href="https://www.facebook.com" style="color:#000" target="_blank">Mindset Group</a></span>	
+			</h4>
+		</div>
+	</div>
+</body>
+</html>
